c69bf7f628f45a2015b9b28fffe50c46f2f458eb
Sebastian Carmona <scarmona04@hotmail.com>
2024-08-04 22:07:15 -0500

    Agregando nginx al proyecto

--- main.tf
+++ main.tf
@@ -3,9 +3,56 @@ provider "aws" {
   region = "us-east-1"
 }
 
-##recurso##
+##Recurso##
 
 resource "aws_instance" "nginx-server" {
   ami = "ami-0ba9883b710b05ac6"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
+  key_name = aws_key_pair.nginx-server-ssh.key_name
+  vpc_security_group_ids = [aws_security_group.nginx-server-sg.id]
+
+}
+
+
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
+}
+
+
+##Security Groups###
+resource "aws_security_group" "nginx-server-sg" {
+  
+  name = "nginx-server-sg"
+  description = "Security Group para mis servidores nginx"
+
+  ingress {
+    to_port = 22
+    from_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    to_port = 80
+    from_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
